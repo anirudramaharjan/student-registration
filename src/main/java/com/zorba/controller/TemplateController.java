fdd666a6227745591cfb4afbf29f8964dd121b65
Nischal Dahal <Nischal Dahal@DESKTOP-UC0L2LJ.lan>
2022-02-18 08:43:36 -0500

    Second commit: UI Updated

--- src/main/java/com/zorba/controller/TemplateController.java
+++ src/main/java/com/zorba/controller/TemplateController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.zorba.model.Student;
 import com.zorba.repository.StudentRepository;
 
 @Controller
@@ -14,11 +15,15 @@ public class TemplateController {
 	@Autowired
 	StudentRepository studentRepo;
 
-	@GetMapping(value = "/form")
-	public String getRegistrationForm() {
+	@GetMapping(value = "/registration")
+	public String getRegistrationForm(Student student) {
 		return "registration";
 	}
 
+	@GetMapping(value ="/")
+	public String home() {
+		return "login";
+	}
 
 
 }
